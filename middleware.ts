e19b38819bc36f26840c29e79eed2cfa042f4922
Anton Yefanov <xt.yefonova@gmail.com>
2024-08-08 23:32:15 +0300

    auth 2
    (cherry picked from commit f017ffefee6889fd284ba81a5a0f0197d268a430)

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 import {
+  DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
   authRoutes,
-  DEFAULT_LOGIN_REDIRECT,
   publicRoutes,
 } from "@/routes";
 
@@ -16,31 +16,6 @@ export default auth((req) => {
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-  const loginRoute = "/auth/login";
-  const registerRoute = "/auth/register";
-
-  if (isApiAuthRoute) {
-    return null;
-  }
-
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return null;
-  }
-
-  if (isLoggedIn) {
-    Response.redirect(new URL("/auth/login", nextUrl));
-  }
-
-  // // TODO: проверить почему infinite loop
-  // if (!isLoggedIn && !isPublicRoute && nextUrl.pathname !== loginRoute && nextUrl.pathname !== registerRoute) {
-  //   return Response.redirect(new URL("/auth/login", nextUrl));
-  // }
-
-  return null;
 });
 
 export const config = {
