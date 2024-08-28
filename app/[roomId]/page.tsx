952e8581d1387b777b91c29d4e8315868d8ff267
Anton Yefanov <xt.yefonova@gmail.com>
2024-08-28 16:30:48 +0300

    save

--- app/[roomId]/page.tsx
+++ app/[roomId]/page.tsx
@@ -1,116 +1,126 @@
-"use client";
+'use client';
 
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { cn } from "@/lib/utils/cn";
-import { useSocket } from "@/infrastructure/providers/SocketProvider";
-import { Badge } from "@/components/ui/Badge";
-import { Textarea } from "@/components/ui/Textarea";
-import { Button } from "@/components/ui/Button";
-import { signOut } from "next-auth/react";
-import { useCurrentUser } from "@/hooks/useCurrentUser";
-import { RoomsDialog } from "@/app/[roomId]/_components/RoomsDialog";
-import { useParams } from "next/navigation";
-import { Chat } from "@/app/[roomId]/_components/Chat";
-import { Message } from "@/types/Message";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { cn } from '@/lib/utils/cn';
+import { useSocket } from '@/infrastructure/providers/SocketProvider';
+import { Badge } from '@/components/ui/Badge';
+import { Textarea } from '@/components/ui/Textarea';
+import { Button } from '@/components/ui/Button';
+import { signOut } from 'next-auth/react';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { RoomsDialog } from '@/app/[roomId]/_components/RoomsDialog';
+import { useParams } from 'next/navigation';
+import { Chat } from '@/app/[roomId]/_components/Chat';
+import { Message } from '@/types/Message';
+import { observer } from 'mobx-react-lite';
 
-export default function Room() {
-  const messagesRef = useRef<HTMLDivElement | null>(null);
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [value, setValue] = useState("");
-  const { socket, isConnected } = useSocket();
-  const currentUser = useCurrentUser();
-  const params = useParams();
+function Room() {
+    const messagesRef = useRef<HTMLDivElement | null>(null);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [value, setValue] = useState('');
+    const { socket, isConnected } = useSocket();
+    const currentUser = useCurrentUser();
+    const params = useParams();
 
-  useEffect(() => {
-    const roomId = Array.isArray(params?.roomId)
-      ? params?.roomId[0]
-      : params?.roomId;
-    if (roomId) {
-      socket?.emit("roomJoined", { roomId, user: currentUser });
-    }
-  }, [params?.roomId, socket, currentUser]);
-
-  const scrollToBottom = () => {
-    if (messagesRef?.current) {
-      setTimeout(() => {
-        if (messagesRef.current) {
-          if ("scrollTop" in messagesRef.current) {
-            messagesRef.current.scrollTop =
-              messagesRef?.current.scrollHeight -
-              messagesRef?.current.clientHeight;
-          }
+    useEffect(() => {
+        const roomId = Array.isArray(params?.roomId)
+            ? params?.roomId[0]
+            : params?.roomId;
+        if (roomId) {
+            socket?.emit('roomJoined', { roomId, user: currentUser });
         }
-      }, 0);
-    }
-  };
+    }, [params?.roomId, socket, currentUser]);
 
-  useEffect(() => {
-    socket?.on("message", (message) => {
-      setMessages((prev) => [...prev, message]);
-    });
-
-    socket?.on("roomJoined", ({ roomId, user }) => {
-      if (user?.id !== currentUser?.id) {
-        setMessages((prev) => [...prev, {sender: "SYSTEM", toRoomId: roomId, text: `${user?.name} joined room`}])
-      }
-    })
-  }, [socket]);
-
-  const sendMessage = useCallback(() => {
-    const newMessage: Message = {
-      text: value,
-      sender: { id: currentUser?.id, name: currentUser?.name },
-      toRoomId: params?.roomId as string,
+    const scrollToBottom = () => {
+        if (messagesRef?.current) {
+            setTimeout(() => {
+                if (messagesRef.current) {
+                    if ('scrollTop' in messagesRef.current) {
+                        messagesRef.current.scrollTop =
+                            messagesRef?.current.scrollHeight -
+                            messagesRef?.current.clientHeight;
+                    }
+                }
+            }, 0);
+        }
     };
-    socket?.emit("message", newMessage);
-    setMessages((prev) => [...prev, newMessage]);
-    setValue("");
-    scrollToBottom();
-  }, [socket, value]);
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      sendMessage();
-    }
-  };
+    useEffect(() => {
+        socket?.on('message', (message) => {
+            setMessages((prev) => [...prev, message]);
+        });
 
-  return (
-    <main className="flex min-h-screen flex-col justify-between p-4">
-      <div className="flex items-center justify-between">
-        <Badge
-          className={cn(
-            "select-none w-fit",
-            isConnected
-              ? "bg-green-800 hover:bg-green-800 "
-              : "bg-yellow-700 hover:bg-yellow-700 ",
-          )}
-        >
-          {isConnected ? "Connected" : "Not connected"}
-        </Badge>
-        <Button onClick={() => signOut({ callbackUrl: "/auth/login" })}>
-          Sign out
-        </Button>
-      </div>
-      <Chat ref={messagesRef} messages={messages} />
-      <div className="flex w-full gap-4">
-        <Textarea
-          onKeyDown={handleKeyDown}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          style={{ resize: "none" }}
-          className="border-blue-300"
-          placeholder="Булкозавр..."
-        />
-        <RoomsDialog />
-        <Button
-          onClick={sendMessage}
-          variant="default"
-          className="h-[60px] w-20 flex-shrink-0"
-        >
-          Send
-        </Button>
-      </div>
-    </main>
-  );
+        socket?.on('roomJoined', ({ roomId, user }) => {
+            if (user?.id !== currentUser?.id) {
+                setMessages((prev) => [
+                    ...prev,
+                    {
+                        sender: 'SYSTEM',
+                        toRoomId: roomId,
+                        text: `${user?.name} joined room`,
+                    },
+                ]);
+            }
+        });
+    }, [socket]);
+
+    const sendMessage = useCallback(() => {
+        const newMessage: Message = {
+            text: value,
+            sender: { id: currentUser?.id, name: currentUser?.name },
+            toRoomId: params?.roomId as string,
+        };
+        socket?.emit('message', newMessage);
+        setMessages((prev) => [...prev, newMessage]);
+        setValue('');
+        scrollToBottom();
+    }, [socket, value]);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            sendMessage();
+        }
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col justify-between p-4">
+            <div className="flex items-center justify-between">
+                <Badge
+                    className={cn(
+                        'select-none w-fit',
+                        isConnected
+                            ? 'bg-green-800 hover:bg-green-800 '
+                            : 'bg-yellow-700 hover:bg-yellow-700 '
+                    )}
+                >
+                    {isConnected ? 'Connected' : 'Not connected'}
+                </Badge>
+                <Button onClick={() => signOut({ callbackUrl: '/auth/login' })}>
+                    Sign out
+                </Button>
+            </div>
+            <Chat ref={messagesRef} messages={messages} />
+            <div className="flex w-full gap-4">
+                <Textarea
+                    onKeyDown={handleKeyDown}
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    style={{ resize: 'none' }}
+                    className="border-blue-300"
+                    placeholder="Булкозавр..."
+                />
+                <RoomsDialog />
+                <Button
+                    onClick={sendMessage}
+                    variant="default"
+                    className="h-[60px] w-20 flex-shrink-0"
+                >
+                    Send
+                </Button>
+            </div>
+        </main>
+    );
 }
+
+export default observer(Room);
